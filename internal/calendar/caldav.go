86d2869589bee6fc5849e571f55bce0b435be3d0
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-04-03 16:41:36 -0700

    wip

--- internal/calendar/caldav.go
+++ internal/calendar/caldav.go
@@ -1,10 +1,13 @@
 package calendar
 
 import (
+	"calendar-summary/internal/tel"
 	"context"
 	"crypto/tls"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/emersion/go-ical"
@@ -66,6 +69,119 @@ func (c Caldav) Calendars(ctx context.Context) ([]Calendar, error) {
 	return out, nil
 }
 
+func (c Caldav) applyRecurrence(end time.Time, ev Event, recurrence string) {
+	type byDayRules struct {
+		Offset   int
+		Weekdays []time.Weekday
+	}
+
+	type recurRules struct {
+		FREQ     string
+		INTERVAL int
+		UNTIL    time.Time
+		COUNT    int
+
+		BYDAY      byDayRules
+		BYMONTHDAY []int
+		BYMONTH    []time.Month
+		BYYEARDAY  []int
+		BYWEEKNO   []int
+		BYHOUR     []int
+		BYMINUTE   []int
+
+		WKST time.Weekday
+	}
+
+	var rules recurRules
+
+	for _, pair := range strings.Split(recurrence, ";") {
+		segments := strings.Split(pair, "=")
+		if len(segments) < 2 {
+			tel.Log.Warn("caldav", "invalid RRULE pair", "pair", pair)
+			continue
+		}
+		key := segments[0]
+		value := segments[1]
+		switch key {
+		case "FREQ":
+			rules.FREQ = value
+		case "INTERVAL":
+			val, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				tel.Log.Error("caldav", "invalid INTERVAL value", "value", value, "err", err)
+				continue
+			}
+			rules.INTERVAL = int(val)
+		case "UNTIL":
+			t, err := time.Parse("20060102T150405Z", value)
+			if err != nil {
+				tel.Log.Error("caldav", "invalid UNTIL value", "value", value, "err", err)
+				continue
+			}
+			rules.UNTIL = t
+		case "COUNT":
+			count, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				tel.Log.Error("caldav", "invalid COUNT value", "value", value, "err", err)
+				continue
+			}
+			rules.COUNT = int(count)
+		case "BYDAY":
+			negative := false
+			offset := ""
+			var weekdays []string
+			for i, c := range value {
+				if c == '-' && i == 0 {
+					negative = true
+					continue
+				}
+				if c >= '0' && c <= '9' {
+					offset += string(c)
+					continue
+				}
+				if c == ',' {
+					weekdays = append(weekdays, "")
+					continue
+				}
+				if c >= 'A' && c <= 'Z' {
+					weekdays[len(weekdays)-1] += string(c)
+					continue
+				}
+			}
+			if offset != "" {
+				parsed, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					tel.Log.Error("caldav", "invalid BYDAY value", "value", value, "err", err)
+					continue
+				}
+				rules.BYDAY.Offset = int(parsed)
+				if negative {
+					rules.BYDAY.Offset *= -1
+				}
+			}
+			for _, w := range weekdays {
+				switch w {
+				case "MO":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Monday)
+				case "TU":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Tuesday)
+				case "WE":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Wednesday)
+				case "TH":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Thursday)
+				case "FR":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Friday)
+				case "SA":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Saturday)
+				case "SU":
+					rules.BYDAY.Weekdays = append(rules.BYDAY.Weekdays, time.Sunday)
+				}
+			}
+		}
+	}
+
+}
+
 func (c Caldav) Events(ctx context.Context, calendar Calendar, start, end time.Time) ([]Event, error) {
 	query := &caldav.CalendarQuery{
 		CompFilter: caldav.CompFilter{
@@ -123,12 +239,17 @@ func (c Caldav) Events(ctx context.Context, calendar Calendar, start, end time.T
 				}
 			}
 
-			out = append(out, Event{
+			ev := Event{
 				Name:  name,
 				Tags:  tags,
 				Start: start,
 				End:   end,
-			})
+			}
+			out = append(out, ev)
+
+			recurrence := e.Props.Get(ical.PropRecurrenceRule)
+			if recurrence != nil {
+			}
 		}
 	}
 
